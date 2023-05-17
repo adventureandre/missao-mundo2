6c2e8d141bc9950810b58af194a8cbff8818bd11
andre <adventureandre@hotmail.com>
2023-05-17 20:54:34 -0300

    projeto sendo perarado para receber o trabalho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
     return (<>
-            <div>Pagina Home aki</div>
+            <div>Pagina Home</div>
             <a href='http://localhost:3000/todo'>Pagina TODO -- click aki!</a>
         </>
     )
